96a1a2cd24a633db469492a1f4d43e4f5433c0ec
kailam-ada <kai_lam@hotmail.fr>
2024-01-22 00:00:16 +0100

    new feat: changed insee code input to an easier one: the post code, using a second API to fetch the insee code from the postal code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # API_Project
+Langages : Javascript, HTML
+Software : Postman
+IDE : VSCode
+Utilisation de l'API Géorisques pour ce projet.
+https://api.gouv.fr/documentation/api-georisques
+
+Le but de ce projet est de pouvoir récupérer des données et de les visualiser sur un site html.
--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 let searchButton = document.querySelector("#search");
-let codeInput = document.querySelector("#code");
+// let codeInput = document.querySelector("#code");
+let postCodeInput = document.querySelector("#postCode");
 let contentDiv = document.querySelector("#content");
 
 searchButton.addEventListener("click", () => {
-    console.log("button pressed");
-    console.log(`${codeInput.value}`);
-    sendApiRequest();
+    // console.log("button pressed");
+    // console.log(`${codeInput.value}`);
+    console.log(`${postCodeInput.value}`);
+    // sendApiRequest();
+    postCodeToInsee();
 });
 
-async function sendApiRequest() {
+
+async function postCodeToInsee() {
     try {
-        let response = await fetch(`https://www.georisques.gouv.fr/api/v1/gaspar/risques?code_insee=${codeInput.value}`);
+        let response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${postCodeInput.value}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
+        
         let data = await response.json();
-        console.log( data);
-        useApiData(data);
+        console.log(data[0].code);
+        sendApiRequest(data);
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-function useApiData(data){
-    contentDiv.innerHTML += `<p>Code Insee : ${data.data[0].code_insee}</p>`
-    contentDiv.innerHTML += `<p>Commune : ${data.data[0].libelle_commune}</p>`
+async function sendApiRequest(data) {
+    try {
+        let response = await fetch(`https://www.georisques.gouv.fr/api/v1/gaspar/risques?code_insee=${data[0].code}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        let riskData = await response.json();
+        console.log(riskData);
+        useApiData(riskData, data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function useApiData(data, secondData){
+    contentDiv.innerHTML = "";
+    contentDiv.innerHTML += `<p>Code Insee : <span class="result">${data.data[0].code_insee}</span></p>`
+    contentDiv.innerHTML += `<p>Commune : <span class="result">${data.data[0].libelle_commune}</span></p>`
+    contentDiv.innerHTML += `<p>Population : <span class="result">${secondData[0].population}</span></p>`
+    contentDiv.innerHTML += `<p>Risques :</p>`
+
     let risquesDetailsArray = data.data[0].risques_detail;
+
     risquesDetailsArray.forEach(risqueDetail => {
         let libelleRisqueLong = risqueDetail.libelle_risque_long;
-        
-        // Display or do something with libelleRisqueLong
-        contentDiv.innerHTML += `<p>${libelleRisqueLong}</p>`;
+        contentDiv.innerHTML += `<p class="result">${libelleRisqueLong}</p>`;
     });
 }
 
-// Assuming 'data' is the response data structure
-
-// Assuming 'contentDiv' is the HTML element where you want to display the results
-
-// Clear the contentDiv before displaying new data
-contentDiv.innerHTML = "";
-
-// Iterate over the array and display libelle_risque_long values
